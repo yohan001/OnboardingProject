bf4a9b1d4c8777ee789f3ef3e1468f80af953f21
yohan <yohansx11@gmail.com>
2021-03-05 16:16:52 +0700

    Add Network State

--- app/src/main/java/com/example/movieapp/NetworkStateActivity.kt
+++ app/src/main/java/com/example/movieapp/NetworkStateActivity.kt
@@ -1,4 +1,24 @@
 package com.example.movieapp
 
-class NetworkStateActivity {
+enum class Status{
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
+class NetworkStateActivity(val status: Status, val msg: String){
+
+    companion object{
+
+        val LOADED :NetworkStateActivity
+        val LOADING : NetworkStateActivity
+        val ERROR : NetworkStateActivity
+
+        init {
+            LOADED = NetworkStateActivity(Status.SUCCESS,msg = "Success")
+            LOADING = NetworkStateActivity(Status.RUNNING,msg = "Running")
+            ERROR = NetworkStateActivity(Status.FAILED,msg = "Failed")
+        }
+
+    }
 }
