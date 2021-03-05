dfe33fff0d53d5b101364c84ddd02d774a08a56e
yohan <yohansx11@gmail.com>
2021-03-05 17:13:30 +0700

    Add Value Object

---
+++ app/src/main/java/com/example/movieapp/ValueObject/MovieDetail.kt
@@ -0,0 +1,26 @@
+package com.example.movieapp.ValueObject
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetail(
+    val budget: Int,
+    val genres: List<Genre>,
+    val id: Int,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val rating: Int,
+)
