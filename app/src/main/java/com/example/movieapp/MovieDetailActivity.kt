72e4adcb7e47df2bd53086cb7f5ecea144ab2107
yohan <yohansx11@gmail.com>
2021-03-05 16:04:26 +0700

    Add Movie Detail

---
+++ app/src/main/java/com/example/movieapp/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R.*
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_movie_detail)
+    }
+}
